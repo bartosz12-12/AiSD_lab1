3772177c7861048da105962a2124500efa001c12
bartosz12-12 <73654110+bartosz12-12@users.noreply.github.com>
2021-11-25 13:06:54 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,77 +1,35 @@
-# Zad1
-# def foo(litera, nazwisko):
-#     return litera + ". "+ nazwisko
-# print(foo("B", "Lipinski"))
+def numbers(n : int):
+    if n < 0:
+        return
+    print(n)
+    numbers(n-1)
 
-# Zad2
-# def foo(imie, nazwisko):
-#     return imie[0] + ". " + nazwisko
-# print(foo("Bartosz", "Lipiński"))
 
-# zad3
-# def foo(liczba1, liczba2, wiek):
-#     return liczba1*100 + liczba2 - wiek
-#
-# print(foo(20, 21, 20))
 
-# zad4
-# def foo(imie, nazwisko):
-#     return imie[0] + ". " + nazwisko
-#
-# def foo2(imie, nazwisko, func):
-#     return  func(imie, nazwisko)
-#
-# print(foo2("Bartosz", "Lipinski",foo))
+def fib(n : int):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
 
-#zad5
-# def foo(a, b):
-#     if a >= 0 and b > 0:
-#         return a/b
-# print(foo(2, 2))
-# print(foo(-1, 1))
+    else:
+        return  fib(n-1) + fib(n-2)
 
-# zad6
-# suma = 0
-# while suma != 100:
-#     a = int(input())
-#     suma +=a
-# print("suma = 100")
+def power(num : int, n : int):
+    if(n == 0):
+        return 1
+    else:
 
-# zad7
-# lista = [1, 2, 3, 4]
-# def foo(lista):
-#     return tuple(lista)
-#
-# print(foo(lista))
+            return num * power(num, n-1)
 
-#zad8
-# lista = []
-# a = 0
-# while  a != "q":
-#     a = input()
-#     if a != "q":
-#         lista.append(a)
-# lista = tuple(lista)
-# print(lista)
-# zad9
-# def foo(a: int):
-#     d = {
-#         1: "Poniedzialek",
-#         2: "Wtorek",
-#         3: "Środa",
-#         4: "Czwartek",
-#         5: "Piątek",
-#         6: "Sobota",
-#         7: "Niedziela"
-#     }
-#     return d[a]
-# print(foo(6))
-# zad10
-def foo(napis):
-    a = True
-    for x in range(len(napis)):
-        if napis[x] != napis[-x-1]:
-            a = False
-    return a
+def reverse(txt):
+    if len(txt) == 0:
+        return txt
+    else:
+        return reverse(txt[1:]) + txt[0]
 
-print(foo("kajak"))
+def factorial(n:int):
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
